c73e72c46a499999e4cf07c2ba224b062b9fe8ef
Kaori Momoki <k_momoki@ga-tech.co.jp>
2018-11-09 17:18:53 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # creates a link of the form http://127.0.0.1:8000/post/1/ with name post_detail, pk = primary key
 ]
