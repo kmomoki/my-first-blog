7c76329f3e2d44e82346f1a98da31c014e25c3ab
Kaori Momoki <k_momoki@ga-tech.co.jp>
2018-11-12 15:11:27 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # creates a link of the form http://127.0.0.1:8000/post/1/ with name post_detail, pk = primary key
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
