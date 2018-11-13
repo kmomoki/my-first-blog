8cc7cad8eae220c25416e549d158c2d85c56a62a
Kaori Momoki <k_momoki@ga-tech.co.jp>
2018-11-13 17:44:54 +0900

    Added publish post and delete post buttons to the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # creates a link of the form http://127.0.0.1:8000/post/1/ with name post_detail, pk = primary key
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
